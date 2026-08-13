34bf1cf2f84ca04b19dbc45cbc8b6d69f3fb0574
Doc1998 <docmp98@gmail.com>
2026-08-13 15:50:53 +0100

    updates

---
+++ src/_data/client.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "name": "Wild Heat Saunas",
+    "email": "test@test.com",
+    "phoneForTel": "+35376712695",
+    "phoneFormatted": "+353 87 671 2695",
+    "address": {
+        "lineOne": "Galway",
+        "lineTwo": "",
+        "city": "Galway",
+        "state": "Leinster and All Ireland",
+        "zip": "",
+        "country": "IE",
+        "mapLink": ""
+    },
+    "socials": {
+        "facebook": "",
+        "instagram": ""
+    },
+    "domain": "wildheatsaunas.ie"
+};
